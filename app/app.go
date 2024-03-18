02a262310090b84f77b682ca478582243895aa5f
Wicho <wvan1901@hotmail.com>
2024-03-17 21:32:18 -0500

    Added Cursor movement

--- app/app.go
+++ app/app.go
@@ -15,6 +15,7 @@ import (
 
 const (
     CONTROLCASCII = 3
+    WHIM_VERSION = "0.0.1"
 )
 
 func RunApp(){
@@ -23,7 +24,7 @@ func RunApp(){
     AppData := initEditor(&oldState)
     for {
         editorRefreshScreen(&AppData)
-        editorProcessKeyPress(&oldState)
+        editorProcessKeyPress(&AppData)
     }
 }
 
@@ -43,13 +44,14 @@ func editorReadKey() byte {
     return input.ReaderByte()//inputByte
 }
 
-func editorProcessKeyPress(oldState *term.State){
-    inputRune, _ := utf8.DecodeRune([]byte(string(editorReadKey())))
+func editorProcessKeyPress(appData *data.EditorConfig){
+    keyRead := editorReadKey()
+    inputRune, _ := utf8.DecodeRune([]byte(string(keyRead)))
     if unicode.IsControl(inputRune){
         switch inputRune {
         case CONTROLCASCII:
             fmt.Println("<C>")
-            disableRawMode(oldState)
+            disableRawMode(&appData.OldTerminalState)
             fmt.Print("\033[2J")
             fmt.Print("\033[H")
             defer os.Exit(0)
@@ -59,7 +61,26 @@ func editorProcessKeyPress(oldState *term.State){
     switch string(inputRune) {
     case "q":
         fmt.Println("<q>")
+    case "h", "j", "k", "l":
+        editorMoveCursor(appData, keyRead)
     default: fmt.Print(string(inputRune))
+    }
+}
+
+func editorMoveCursor(appData *data.EditorConfig, inputByte byte){
+    switch string(inputByte) {
+        case "h":
+            //Left
+            appData.CursorPosX--
+        case "j":
+            //Down
+            appData.CursorPosY++
+        case "k":
+            //Up
+            appData.CursorPosY--
+        case "l":
+            //Right
+            appData.CursorPosX++
     }
 }
 
@@ -67,12 +88,16 @@ func editorRefreshScreen(appData *data.EditorConfig){
     // fmt.Print("\033[2J")
     // fmt.Print("\033[H")
     appData.ABuf.WriteString("\033[?25l")
-    appData.ABuf.WriteString("\033[2J")
+    //appData.ABuf.WriteString("\033[2J")
     appData.ABuf.WriteString("\033[H")
     editorDrawRows(appData)
     // fmt.Print("\033[H")
-    appData.ABuf.WriteString("\033[H")
+    // appData.ABuf.WriteString("\033[H")
+    
+    setCursorPosition(appData)
+
     appData.ABuf.WriteString("\033[?25h")
+
     fmt.Print(appData.ABuf.String())
     //setCursorPosition(appData)
     appData.ABuf.Reset()
@@ -87,11 +112,29 @@ func die(){
 
 func editorDrawRows(editorData *data.EditorConfig){
     for y:=0; y<editorData.ScreenRows; y++ {
-        // fmt.Print("~")
-        editorData.ABuf.WriteString("~")
+        if y == editorData.ScreenRows/3 {
+            welcome := fmt.Sprintf("Whim Editor -- version %s", WHIM_VERSION)
+            if len(welcome) > editorData.ScreenColumns{
+                welcome = "v:"+WHIM_VERSION
+            }
+            padding := (editorData.ScreenColumns - len(welcome))/2
+            if padding>0{
+                editorData.ABuf.WriteString("~")
+                padding--
+            }
+            for padding>0 {
+                padding--
+                editorData.ABuf.WriteString(" ")
+            }
+            editorData.ABuf.WriteString(welcome)
+        } else {
+            editorData.ABuf.WriteString("~")
+        }
+        // editorData.ABuf.WriteString("~")
+
+        editorData.ABuf.WriteString("\033[K")
         if y < editorData.ScreenRows -1 {
             editorData.ABuf.WriteString("\r\n")
-            // fmt.Print("\r\n")
         }
     }
 }
@@ -107,7 +150,7 @@ func getWindowSize()(int, int){
 
 func initEditor(oldState *term.State) data.EditorConfig{
     width, height := getWindowSize()
-    initCursorX, initCursorY := 7,7
+    initCursorX, initCursorY := 2,2
     //fmt.Printf("\033[%d;%dH", initCursorY, initCursorX) // Set cursor position    
     var newBuf strings.Builder
     // newBuf.WriteString("")
@@ -133,5 +176,7 @@ func getCursorPosition(data *data.EditorConfig) (int, int){
     return data.CursorPosX, data.CursorPosY
 }
 func setCursorPosition(data *data.EditorConfig) {
-    fmt.Printf("\033[%d;%dH", data.CursorPosY, data.CursorPosX) // Set cursor position    
+    //fmt.Printf("\033[%d;%dH", data.CursorPosY, data.CursorPosX) // Set cursor position    
+    cursorPos := fmt.Sprintf("\033[%d;%dH", data.CursorPosY, data.CursorPosX)
+    data.ABuf.WriteString(cursorPos)
 }
