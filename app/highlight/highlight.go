d47255a3c0afead310402ff5e412b3a46277f921
Wicho <wvan1901@hotmail.com>
2024-05-27 20:10:26 -0500

    Colorful Numbers

--- app/highlight/highlight.go
+++ app/highlight/highlight.go
@@ -1,19 +1,45 @@
 package highlight
 
 import (
-    "unicode"
+	"strings"
+	"unicode"
 	"wicho/whim/app/consts"
 )
 
 func EditorUpdateSyntax(curRow *consts.EditorRow){
     rowHl := make([]int, curRow.RenderSize)
     renderRunes := []rune(*curRow.Render)
-    for i := 0; i < curRow.RenderSize; i++ {
-        if unicode.IsDigit(renderRunes[i]){
-            rowHl[i] = consts.HL_NUMBER
+
+    prevSeperator := true
+
+    i := 0
+    for i < curRow.RenderSize {
+        curRune := renderRunes[i]
+        prevHl := consts.HL_NORMAL
+        if i > 0 {
+            prevHl = rowHl[i-1]
+        } else {
+            prevHl = consts.HL_NORMAL
         }
+
+        if unicode.IsDigit(curRune) && (prevSeperator || prevHl == consts.HL_NUMBER) || (curRune == '.' && prevHl == consts.HL_NUMBER) {
+            rowHl[i] = consts.HL_NUMBER
+            i++
+            prevSeperator = false
+            continue
+        }
+
+        prevSeperator = isSeperator(curRune)
+        i++
     }
     curRow.Highlights = rowHl
+}
+
+func isSeperator(aRune rune) bool {
+    isRuneSpace := unicode.IsSpace(aRune)
+    isEmpty := aRune == '\u0000'
+    isInvalid := strings.ContainsRune(",.()+-/*=~%<>[];", aRune)
+    return isRuneSpace || isEmpty || isInvalid
 }
 
 func EditorSyntaxToColor(hl int) int {
